c540b941d0385f90d687e3a7e5bda0120b236355
Florian Boisen <florianboisen@hotmail.de>
2025-03-11 06:37:23 +0100

    wallpaper: only execute swww when wallpaper actually changes

--- .config/hypr/scripts/wallpaper.py
+++ .config/hypr/scripts/wallpaper.py
@@ -17,22 +17,38 @@ latidute = 51.050407
 longitude = 13.737262
 city = LocationInfo("Dresden", "Germany", "Europe/Berlin", latidute, longitude)
 
-while True:
-    if now() < blue_hour(city.observer)[0]:
-        subprocess.run(f"{WALLPAPER_CMD} {IMG_NIGHT}", shell=True)
-        print("NIGHT 1")
-    elif now() < golden_hour(city.observer)[1]:
-        subprocess.run(f"{WALLPAPER_CMD} {IMG_MORNING}", shell=True)
-        print("MORNING")
-    elif now() < golden_hour(city.observer, direction=SunDirection.SETTING)[0]:
-        subprocess.run(f"{WALLPAPER_CMD} {IMG_NOON}", shell=True)
-        print("NOON")
-    elif now() < blue_hour(city.observer, direction=SunDirection.SETTING)[1]:
-        subprocess.run(f"{WALLPAPER_CMD} {IMG_EVENING}", shell=True)
-        print("EVENING")
-    else:
-        subprocess.run(f"{WALLPAPER_CMD} {IMG_NIGHT}", shell=True)
-        print("NIGHT 2")
-    
-    time.sleep(60)
+current_wallpaper = None
 
+if now() < blue_hour(city.observer)[0]:
+    current_wallpaper = IMG_NIGHT
+elif now() < golden_hour(city.observer)[1]:
+    current_wallpaper = IMG_MORNING
+elif now() < golden_hour(city.observer, direction=SunDirection.SETTING)[0]:
+    current_wallpaper = IMG_NOON
+elif now() < blue_hour(city.observer, direction=SunDirection.SETTING)[1]:
+    current_wallpaper = IMG_EVENING
+else:
+    current_wallpaper = IMG_NIGHT
+
+subprocess.run(f"{WALLPAPER_CMD} {current_wallpaper}", shell=True)
+print(f"Set wallpaper to {current_wallpaper}")
+
+while True:
+    next_wallpaper = None
+    if now() < blue_hour(city.observer)[0]:
+        next_wallpaper = IMG_NIGHT
+    elif now() < golden_hour(city.observer)[1]:
+        next_wallpaper = IMG_MORNING
+    elif now() < golden_hour(city.observer, direction=SunDirection.SETTING)[0]:
+        next_wallpaper = IMG_NOON
+    elif now() < blue_hour(city.observer, direction=SunDirection.SETTING)[1]:
+        next_wallpaper = IMG_EVENING
+    else:
+        next_wallpaper = IMG_NIGHT
+   
+    if next_wallpaper != current_wallpaper:
+        current_wallpaper = next_wallpaper
+        subprocess.run(f"{WALLPAPER_CMD} {current_wallpaper}", shell=True)
+        print(f"Set wallpaper to {curent_wallpaper}")
+
+    time.sleep(1)
