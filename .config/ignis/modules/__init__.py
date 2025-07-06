36f1cb86724bfe86c80d27a06b9f49620a80149c
Florian Boisen <florianboisen@hotmail.de>
2025-07-06 16:25:36 +0200

    Start new era

--- .config/ignis/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-from .statusbar import StatusBar 
-from .notification import NotificationPopupList
-from .control_center import ControlCenter
-
-__all__ = [
-    "StatusBar",
-    "NotificationPopupList",
-    "ControlCenter",
-]
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,2 @@
-print("I may be an idiot, but I am not stupid!")
-
 require("config.keymaps")
 require("config.options")
-require("config.lazy")
